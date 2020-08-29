ad5230973ca3737ed988216457064b9cfee1eac8
parv29 <parvagarwal987@gmail.com>
2020-08-30 01:50:44 +0530

    first commit

---
+++ linear.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 30 01:45:15 2020
+
+@author: Parv
+"""
+
+from numpy import *
+
+# y = mx + b
+# m is slope, b is y-intercept
+def compute_error_for_line_given_points(b, m, points):
+    totalError = 0
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        totalError += (y - (m * x + b)) ** 2
+    return totalError / float(len(points))
